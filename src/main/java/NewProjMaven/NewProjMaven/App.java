7bb53a5424b18adae5f82ea0f79faf757dae22f2
NikhilOpsera <nikhil@opsera.io>
2023-09-01 18:18:19 +0530

    firstCommit

---
+++ src/main/java/NewProjMaven/NewProjMaven/App.java
@@ -0,0 +1,13 @@
+package NewProjMaven.NewProjMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
